df20ea07aedcecdf9bd60c47756bf71fefdac239
EricTeo <zcteo.cn@gmail.com>
2021-02-26 14:35:05 +0800

    增加Back to top

---
+++ assets/js/back_to_top.js
@@ -0,0 +1,41 @@
+window.onload = function () {
+    var backToTop = document.getElementById("back-to-top");
+    //文档高度
+    var clientHeight = document.documentElement.clientHeight;
+    var time = null;
+    var isTop = true,
+        cancelScroll = false;
+
+    window.onscroll = function () {
+
+        var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+        if (osTop >= clientHeight) {
+            backToTop.style.display = 'block';
+        } else {
+            backToTop.style.display = 'none';
+        }
+        if (!isTop) {
+            clearInterval(time);
+        }
+        isTop = false;
+    }
+
+    backToTop.onclick = function () {
+        //点击事件
+        if (cancelScroll == false) {
+            time = setInterval(function () {
+                var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+                // 这样子动画会越来越慢
+                var speed = Math.floor(-osTop / 5);
+                document.documentElement.scrollTop = document.body.scrollTop = osTop + speed;
+                isTop = true;
+                if (osTop == 0) {
+                    clearInterval(time);
+                }
+            }, 10);
+        } else {
+            clearInterval(time);
+            cancelScroll = true;
+        }
+    }
+}
