8ec76ce09b76d72ba26335a2930cf9c6b256b96f
EricTeo <zcteo.cn@gmail.com>
2024-01-11 21:54:07 +0800

    博客名称改为闻道

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,25 +5,25 @@ cd "${SCRIPT_DIR}/.." || exit 1
 cp -ar blog blog.bak
 cp blog.md blog.md.bak
 
-cat <<EOF >tmp_header
----
-layout: default
----
-
-EOF
-
 while IFS= read -r line; do
     if [[ $line =~ .*\(blog/.*md\) ]]; then
-        file=$(echo "$line" | grep -o 'blog/.*md')
+        file="$(echo "$line" | grep -o 'blog/.*md')"
+        title="$(echo "$line" | grep -o '\[.*\]' | sed -e 's/\[//g' -e 's/\]//g')"
+        (
+            echo '---'
+            echo 'layout: default'
+            echo "title: '${title}'"
+            echo '---'
+            echo ''
+        ) >tmp_header
         cat tmp_header "$file" >>tmp_md
         sed -e 's#md)#html)#g' -i tmp_md
         mv tmp_md "$file"
+        rm tmp_header
     fi
 done <blog.md
 
 sed -e 's#(blog#(/blog#g' -e 's#md)#html)#g' -i blog.md
-
-rm tmp_header
 
 export LC_ALL=en_US.UTF-8
 
